c9263afaaeb40c79524692268eb13e3f9d44ed59
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-05 12:54:39 +0100

    Update To the Power of _____.py

--- python/edabit/To the Power of _____.py
+++ python/edabit/To the Power of _____.py
@@ -1,2 +1,15 @@
 def calculate_exponent(num, exp):
 	return num**exp
+
+#or
+
+def calculate_exponent(*args):
+	return args[0]**args[1]
+
+#or
+
+from functools import reduce
+import operator
+
+def calculate_exponent(*args):
+	return reduce(operator.pow, args)
