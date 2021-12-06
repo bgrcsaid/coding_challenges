f71cd6177df5f44236c272d263eb9634f6506b9d
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-06 13:34:24 +0100

    Create Is the Number Less than or Equal to Zero.py

---
+++ python/edabit/Is the Number Less than or Equal to Zero.py
@@ -0,0 +1,14 @@
+def less_than_or_equal_to_zero(num):
+	if num <=0:
+		return True
+	else:
+		return False
+
+#or
+
+def less_than_or_equal_to_zero(num):
+  return num <= 0
+
+#or
+
+less_than_or_equal_to_zero = lambda x: x <= 0
