f2ee977c31b37b81e37edae35dd6d22c0509026c
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-08 09:54:37 +0100

    Create Two Makes Ten.py

---
+++ python/edabit/Two Makes Ten.py
@@ -0,0 +1,14 @@
+def makes10(a, b):
+	if a+b==10 or a==10 or b==10:
+		return True
+	else:
+		return False
+
+#or
+
+def makes10(a, b):
+  return 10 in [a, b, a+b]
+
+#or
+
+makes10=lambda a,b : 10 in [a,b,a+b]
