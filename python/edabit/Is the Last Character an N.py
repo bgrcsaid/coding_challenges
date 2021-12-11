e6efb99c2570ce41480425470505a282cdc0fadc
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-11 16:20:23 +0100

    Create Is the Last Character an N.py

---
+++ python/edabit/Is the Last Character an N.py
@@ -0,0 +1,12 @@
+def is_last_character_n(word):
+	return word[-1] == 'n'
+
+#or
+
+def is_last_character_n(word):
+	return word.endswith('n')
+
+#or
+
+def is_last_character_n(word):
+	return word[-1].lower() == 'n'
