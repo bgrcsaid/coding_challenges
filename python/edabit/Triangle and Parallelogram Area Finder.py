d1c5f3a7953ddee177bb991f5f8d9324a0521263
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-13 21:51:09 +0100

    Create Triangle and Parallelogram Area Finder.py

---
+++ python/edabit/Triangle and Parallelogram Area Finder.py
@@ -0,0 +1,15 @@
+def area_shape(base, height, shape):
+	if shape == "triangle":
+		return 0.5 * base * height
+	else:
+		return base * height
+
+#or
+
+def area_shape(b,h,f):
+	return b*h*0.5 if f=='triangle' else b*h
+
+#or
+
+def area_shape(base, height, shape):
+	return base * height / [1, 2][shape == "triangle"]
