9de0033aa3a161f013e922c23d6741ed862ebd0d
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-16 21:15:10 +0100

    Create The 3 Programmers Problem.py

---
+++ python/edabit/The 3 Programmers Problem.py
@@ -0,0 +1,13 @@
+def programmers(one, two, three):
+	return max(one, two, three) - min(one, two, three)
+
+#or
+
+def programmers(*args):
+	return max(args) - min(args)
+
+#or
+
+def programmers(one, two, three):
+	lst=[one,two,three]
+	return(max(lst)-min(lst))
