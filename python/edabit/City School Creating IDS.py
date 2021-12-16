98e7405ec16e2a4fbb0fd9a83c64cd6a75da8ff0
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-16 21:32:57 +0100

    Create City School Creating IDS.py

---
+++ python/edabit/City School Creating IDS.py
@@ -0,0 +1,7 @@
+def create_id(firstname, lastname):
+	return firstname[0].lower()+lastname[0].upper()+lastname[1:3].lower()
+
+#or
+
+def create_id(firstname, lastname):
+	return firstname[0].lower() + lastname[:3].capitalize()
