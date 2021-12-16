aeb9c44e06e9ae5d4a1ce84a26363b33cb734930
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-16 21:32:35 +0100

    Create Many Operators!.py

---
+++ python/edabit/Many Operators!.py
@@ -0,0 +1,17 @@
+def operate(num1, num2, operator):
+	return eval(str(num1)+operator+str(num2))
+
+#or
+
+import operator
+def operate(n1,n2,o):
+	ops = {
+    '+' : operator.add,
+    '-' : operator.sub,
+    '*' : operator.mul,
+    '/' : operator.truediv,  
+    '%' : operator.mod,
+    '^' : operator.xor,
+		}
+		
+	return ops[o](n1,n2)
