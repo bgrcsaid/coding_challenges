5c58c4a1dae586b1b55df28262ceaecf4181c98a
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-17 20:16:30 +0100

    Create Return Last Item.py

---
+++ python/edabit/Return Last Item.py
@@ -0,0 +1,12 @@
+def last_ind(lst):
+	return lst[-1] if lst!=[] and lst!="" else None
+
+#or
+
+def last_ind(lst):
+	return lst[-1] if lst else None
+
+#or
+
+def last_ind(lst):
+	return None if len(lst) == 0 else lst[-1]
