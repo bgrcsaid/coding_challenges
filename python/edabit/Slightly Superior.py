f35c82005725bea50efcbc7168d91c9120dc7750
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-19 12:40:23 +0100

    Create Slightly Superior.py

---
+++ python/edabit/Slightly Superior.py
@@ -0,0 +1,10 @@
+def is_first_superior(lst1, lst2):
+  for i in range(len(lst1)):
+    if lst1[i] > lst2[i]:
+      return True
+  return False
+
+#or
+
+def is_first_superior(lst1, lst2):
+	return lst1 > lst2
