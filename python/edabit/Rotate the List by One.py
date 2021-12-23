cebd4cb56d308bf6784164c7d735cc5d76701175
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-23 18:29:11 +0100

    Create Rotate the List by One.py

---
+++ python/edabit/Rotate the List by One.py
@@ -0,0 +1,13 @@
+def rotate_by_one(lst):
+    return lst[-1:] + lst[:-1]
+
+#or
+
+def rotate_by_one(lst):
+    return [lst[-1]]+lst[0:-1]
+
+#or
+
+def rotate_by_one(lst):
+    lst.insert(0, lst.pop())
+    return lst
