5cc3511f4debee004c20b2104bf0007dc543aeed
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-25 16:32:50 +0100

    Update Fix the Broken Code.py

--- python/edabit/Fix the Broken Code.py
+++ python/edabit/Fix the Broken Code.py
@@ -1,9 +1,2 @@
 def sort_word(word):
-	word = word.split('')
-	word = list(word.sort())
-	final_word = ''
-
-	for char in word
-		final_word = final_word + char
-
-	return final_word
+	return ''.join(sorted(word))
