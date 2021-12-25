286672b87e9d43d9c19c0c22dcbaf4918fa5e62e
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-25 16:32:53 +0100

    Update Height of an Equilateral Triangle.py

--- python/edabit/Height of an Equilateral Triangle.py
+++ python/edabit/Height of an Equilateral Triangle.py
@@ -1,4 +1,9 @@
 import math
 
 def height(side):
-	return str(round(math.sqrt(3)/2*side*10, 1)
+	return str(round(math.sqrt(3)/2*side*10, 1)) + ' mm'
+
+#or
+
+def height(side):
+	return '{:.1f} mm'.format(8.66 * side)
