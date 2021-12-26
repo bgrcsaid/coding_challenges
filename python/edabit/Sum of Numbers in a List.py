228293bd99a0548e662f257d96dc15f3c38c8ffc
bgrcsaid <67913012+bgrcsaid@users.noreply.github.com>
2021-12-26 19:14:47 +0100

    Create Sum of Numbers in a List.py

---
+++ python/edabit/Sum of Numbers in a List.py
@@ -0,0 +1,16 @@
+import math
+
+def list_sum(lst):
+    return sum(i**2 for i in lst if i%2==0) + round(sum(math.sqrt(i) for i in lst if i%2!=0),2)
+
+#or
+
+import math
+
+def list_sum(lst):
+	return round(sum(i**2 if i%2==0 else math.sqrt(i) for i in lst), 2)
+
+#or
+
+def list_sum(lst):
+    return round(sum(i ** .5 if i % 2 else i ** 2 for i in lst), 2)
